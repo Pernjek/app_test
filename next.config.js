9b44efb40aeea3aa316e743ad01aea36afc27c81
pernjek <pernjek.anto@gmail.com>
2023-09-22 15:43:26 +0200

    update next config

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
     apiUrl:
       process.env.NODE_ENV === "development"
         ? "http://localhost:3000/api" // development api
-        : "http://localhost:3000/api", // production api
+        : "app-test-iota.vercel.app/api", // production api
   },
 };
 
