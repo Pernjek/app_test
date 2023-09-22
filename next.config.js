84c2baa919fa82e9c10f74d04ea802c7d7e0be8b
pernjek <pernjek.anto@gmail.com>
2023-09-22 15:39:31 +0200

    config files

--- next.config.js
+++ next.config.js
@@ -1,21 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    serverRuntimeConfig: {
-        dbConfig: {
-            host: 'sql.freedb.tech',
-            port: 3306,
-            user: 'freedb_number_guessing',
-            password: '#b!?2WRs*%4fCNG',
-            database: 'freedb_number_guessing'
-        },
-        secret: 'app12341234app'
+  reactStrictMode: true,
+  serverRuntimeConfig: {
+    dbConfig: {
+      host: process.env.DB_HOST,
+      port: process.env.PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
     },
-    publicRuntimeConfig: {
-        apiUrl: process.env.NODE_ENV === 'development'
-            ? 'http://localhost:3000/api' // development api
-            : 'http://localhost:3000/api' // production api
-    }
-}
+    secret: "app12341234app",
+  },
+  publicRuntimeConfig: {
+    apiUrl:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000/api" // development api
+        : "http://localhost:3000/api", // production api
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
