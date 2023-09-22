6da35cab17749786865b1f1cd70067322731c62f
pernjek <pernjek.anto@gmail.com>
2023-09-22 17:07:43 +0200

    next config update

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   serverRuntimeConfig: {
     dbConfig: {
-      host: process.env.DB_HOST,
-      port: process.env.PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      host: "numbers.cui4gprfreze.eu-north-1.rds.amazonaws.com",
+      port: 3306,
+      user: "number_guessing",
+      password: "number_guessing",
+      database: "numbers",
     },
     secret: "app12341234app",
   },
