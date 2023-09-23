81e1fe8cf503650636a53947ca304959e4920170
pernjek <pernjek.anto@gmail.com>
2023-09-23 15:41:00 +0200

    update db.js

--- helpers/api/db.js
+++ helpers/api/db.js
@@ -24,6 +24,11 @@ async function initialize() {
 
   // connect to db
   const sequelize = new Sequelize(database, user, password, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     dialect: "mysql",
   });
 
